8b128a532ab115fa9e1f691ea6843ba3ccae0353
Ubaydaht <ubaidatolasunkanmi58@gmail.com>
2026-05-10 21:27:53 -0700

    changes

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,27 +1,35 @@
-const express = require("express")
-const cors = require('cors')
-const path =require("path")
+const fs = require("fs");
+const path = require("path");
 const multer = require("multer");
-const app = express();
+
+// ensure uploads folder exists
+const uploadPath = path.join(__dirname, "../uploads");
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-  destination:  (req, file, cb) => {
-    cb(null, "uploads/");
+  destination: (req, file, cb) => {
+    cb(null, uploadPath);
   },
-  filename: (req, file, cb) =>{
+
+  filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
   }
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png"
+  ) {
     cb(null, true);
   } else {
     cb(new Error("Only JPEG and PNG files are allowed"), false);
   }
 };
 
-const upload = multer({storage, fileFilter});
-
+const upload = multer({ storage, fileFilter });
 
 module.exports = upload;
