9cab69120ca8774fd098b01083214c220a68f4d0
Ubaydaht <ubaidatolasunkanmi58@gmail.com>
2026-05-13 23:33:01 -0700

    Add changes to email

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -51,8 +51,21 @@ const postSignup = (req, res) => {
                 let mailOptions = {
                     from:  `"IdeaForge" <${process.env.EMAIL}>`,
                     to: user.email,
-                    subject: 'Hi, Welcome to IdeaForge',
-                    html: `<h1>Welcome</h1>`
+                    subject: `Hi ${user.firstname},`,
+                    html: ` <div style="background-color: #f4f4f4; padding: 0 0 10px; border-radius: 30px 30px 0 0  ;">
+                            <div style="padding-top: 20px; height: 100px; border-radius: 30px 30px 0 0 ; background: linear-gradient(-45deg, #003D9B 0%, #013c958c 100% );">
+                                <h1 style="color:white; text-align: center;">Welcome to IdeaForge!</h1>
+                            </div>
+                            <div style="padding: 30px 0; text-align: center;">
+                                <p style="font-size: 18px;"><span style="font-weight: 600;">Congratulations!</span> Your sign-up was successful!</p>
+                                <p> We are excited to have you on board. Cheers to more great ideas! collaboration and innovation await you.</p>
+                                <div style="padding: 20px 0;">
+                                    <hr style="width: 50%;">
+                                    <p style="margin-bottom: 10px;">Best Regards</p>
+                                    <p style="color: #68FADD; margin-top: 0;">Ubaydah, Team IdeaForge</p>
+                                </div>
+                            </div>
+                        </div>`
                 };
 
                 transporter.sendMail(mailOptions, (error, info) => {
